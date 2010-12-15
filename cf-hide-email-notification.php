507b6c16ae9d842e055251a271f608e74e6c906e
sklein <sklein@546ca710-db30-0410-8887-d9dd8e66aff0>
2010-12-15 20:47:07 +0000

    cleanup of the code, adding comments around functions, and adding a filter to block the welcome email
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-plugins/cf-hide-email-notification/trunk@31793 546ca710-db30-0410-8887-d9dd8e66aff0

--- cf-hide-email-notification.php
+++ cf-hide-email-notification.php
@@ -1,16 +1,21 @@
 <?php
 /*
 Plugin Name: CF Hide Email Notification
-Description: This plugin will hide the nonconfirmation checkbox. It will then force no notification for user creation.
-This is currently setup to work on a multisite install.
-Version: 0.1
+Description: Plugin for handling the blocking of Email confirmations for new users in a MultiSite install of WordPress.
+Version: 1.0
 Author: Crowd Favorite
 Author URI: http://crowdfavorite.com
 */
 
+/**
+ * Request Handler for functions related to this plugin
+ *
+ * @return void
+ */
 function cfhide_request_handler() {
-	if (!empty($_GET['cfhide_action'])) {
-		switch ($_GET['cfhide_action']) {
+	// Get the JS File if it is asked for
+	if (!empty($_GET['cf_action'])) {
+		switch ($_GET['cf_action']) {
 			case 'cfhide_js':
 				cfhide_js();
 				die();
@@ -18,15 +23,31 @@ function cfhide_request_handler() {
 			default:
 				break;
 		}
-	}	
+	}
+	
+	// If the form is posted from the New User page, modify the $_POST
+	if (!empty($_REQUEST['action']) && $_REQUEST['action'] == 'adduser') {
+		check_admin_referer('add-user');
+		$_POST['noconfirmation'] = 1;
+	}
 }
-add_action('admin_init', 'cfhide_request_handler');
+add_action('init', 'cfhide_request_handler', 1);
 
+/**
+ * Enqueue the JS for the New User screen
+ *
+ * @return void
+ */
 function cfhide_enqueue_js() {
-	wp_enqueue_script('cfhide_js', admin_url('?cfhide_action=cfhide_js'), array('jquery') );
+	wp_enqueue_script('cfhide_js', admin_url('?cf_action=cfhide_js'), array('jquery') );
 }
 add_action('admin_print_scripts-user-new.php', 'cfhide_enqueue_js');
 
+/**
+ * JS to be added to the New User screen to hide the noconfirmation checkbox
+ *
+ * @return void
+ */
 function cfhide_js() {
 	// Using JS to check and hide, essentially removes the checked 
 ?>
@@ -36,14 +57,17 @@ jQuery(function($) {
 <?php
 }
 
-// Force nonconfirmation
-function cfhide_force_nonconfirmation() {
-	if ( isset($_REQUEST['action']) && 'adduser' == $_REQUEST['action'] ) {
-		check_admin_referer('add-user');
-		$_POST['noconfirmation'] = 1;
-	}
+/**
+ * This function will block the Welcome email from being sent to the user
+ *
+ * @param int $user_id 
+ * @param string $password 
+ * @param array $meta 
+ * @return bool
+ */
+function cfhide_email_confirmation($user_id = '', $password = '', $meta = '') {
+	return false;
 }
-add_action('admin_init', 'cfhide_force_nonconfirmation');
-
+add_filter('wpmu_welcome_user_notification', 'cfhide_email_confirmation', 10, 3);
 
 ?>
